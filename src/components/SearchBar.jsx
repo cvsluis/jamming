2e83ba296937ebfbc72c4d8df489eeb4a1a4fb8a
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-01 13:55:42 -0700

    feat: add search bar styling

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import '../styles/SearchBar.scss'
 
 const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <form>
+      <input type="text" name="search-bar" id="search-bar" placeholder='Search for a song'></input>
+    </form>
   );
 };
 
