85af4f2359ff1d9d2bb08becf67159434c257fc1
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-11-01 20:38:57 -0700

    feat: add state for playlist title

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/Playlist.scss'
 
 const Playlist = () => {
+  const [playlistTitle, setPlaylistTitle] = useState('');
+  
   return (
     <div className="article-container">
-      <h2>Playlist</h2>
+      <input placeholder='Playlist Name' className='playlist-input' value={playlistTitle} onChange={(e) => setPlaylistTitle(e.target.value)}></input>
     </div>
   );
 };
