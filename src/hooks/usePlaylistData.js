3bc690337ff6d2209f4871202ba220fcdca29867
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-12-13 21:38:53 -0800

    feat: create empty playlist and add to spotify

--- src/hooks/usePlaylistData.js
+++ src/hooks/usePlaylistData.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
+import useProfileData from './useProfileData';
 
 export default function usePlaylistData() {
+  const { accessToken, userId } = useProfileData();
+
   // Playlist Songs State
   const [playlist, setPlaylist] = useState([]);
   const updatePlaylist = (song) => {
@@ -22,5 +25,32 @@ export default function usePlaylistData() {
     setPlaylistTitle(e.target.value);
   };
 
-  return { updatePlaylist, isInPlaylist, playlist, updatePlaylistTitle, playlistTitle };
+  const createPlaylist = async () => {
+    const response = await fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+      headers: {
+        'Authorization': "Bearer " + accessToken
+      },
+      method: 'POST',
+      body: JSON.stringify({ name: playlistTitle })
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.json();
+  };
+
+  const onSubmitPlaylist = async (e) => {
+    e.preventDefault();
+    try {
+      const newPlaylist = await createPlaylist();
+
+      const playlistId = newPlaylist.id;
+    } catch (error) {
+      console.error('Sorry, we could not complete your request: ', error);
+      throw error;
+    }
+  }
+
+  return { updatePlaylist, isInPlaylist, playlist, updatePlaylistTitle, playlistTitle, onSubmitPlaylist };
 }
--- src/hooks/useProfileData.js
+++ src/hooks/useProfileData.js
@@ -3,7 +3,25 @@ import { useEffect, useState } from "react";
 export default function useProfileData() {
   const [accessToken, setAccessToken] = useState('');
 
+  const [userId, setUserId] = useState('');
+
   const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+
+  const getUserId = () => {
+    fetch('https://api.spotify.com/v1/me', {
+      headers: {
+        'Authorization': "Bearer " + accessToken
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => setUserId(data.id))
+      .catch(error => console.error('Error fetching data:', error));
+  };
 
   useEffect(() =>{
     if (accessTokenMatch) {
@@ -11,5 +29,11 @@ export default function useProfileData() {
     }
   }, []);
 
-  return { accessToken };
+  useEffect(() => {
+    if (accessToken) {
+      getUserId();
+    }
+  }, [accessToken]);
+
+  return { accessToken, userId };
 }
