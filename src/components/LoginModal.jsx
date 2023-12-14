c65f4b2b2f6503dc627a7881cf91a8904e00b43d
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-12-13 22:54:25 -0800

    feat: style login modal

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -7,8 +7,9 @@ const LoginModal = () => {
 
   return (
     <div className="login-modal">
-      <h1>Login to Spotify</h1>
-      <a href={`https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`}>Go to Spotify</a>
+      <img src='Spotify.png'></img>
+      <h1>Connect to Spotify</h1>
+      <a href={`https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`}>Get started</a>
     </div>
   )
 }
